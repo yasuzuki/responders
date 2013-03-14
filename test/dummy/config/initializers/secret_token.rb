38683af231b28074e19302191359ec1fcd8e7adc
Yasunori Suzuki <railsyas@gmail.com>
2013-03-15 07:22:28 +0900

    Created new gem named responders.

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'f7f270dfae3c6d5697cdbb3827d2c9f75a7c61146cc0513ef96903a5902e97c61f113a5aca33f0b034a2e1e45f8ba7a77c2c80a701f9838da53b208102cdcc40'
