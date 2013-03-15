6ea4dab388aa6326f834fa79fa3b400cc330fa1c
Yasunori Suzuki <railsyas@gmail.com>
2013-03-15 22:46:07 +0900

    Recreated new gem named responders as rails 3.2.12

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'ebcafededd8025763529076339e4178331ea57080badde56ee4784342ba0a29243ef35c4dda9a2904553e2f1ae155c54b4d20a907192a87b3bf5ff15d4ec3e23'
