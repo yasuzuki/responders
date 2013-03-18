8d80801e130fb512331376706e8de3ca205b54de
Yasunori Suzuki <railsyas@gmail.com>
2013-03-19 06:53:04 +0900

    Added http-cache.

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :updated_at
   before_destroy do
     if self.name == "Undestroyable"
       self.errors.add(:base, "is undestroyable")
